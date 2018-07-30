c583d5a64eb2626ab7f932c6d262704c07f096ce
faissalMT <faissal@madetech.com>
2018-07-30 11:37:30 +0100

    Acceptance test doesn't need gateway mocking

--- spec/acceptance/homes_england/get_returns_spec.rb
+++ spec/acceptance/homes_england/get_returns_spec.rb
@@ -4,10 +4,53 @@ require_relative '../shared_context/use_case_factory'
 describe 'Getting multiple returns' do
   include_context 'use case factory'
 
-  let(:all_returns) {
-    [{
-      project_id: 1,
-      data:
+  let(:returns_for_project_1) {
+    [
+      {
+        project_id: 1,
+        data:
+        {
+          summary: {
+            project_name: 'Cats Protection League',
+            description: 'A new headquarters for all the Cats',
+            lead_authority: 'Made Tech'
+          },
+          infrastructure: {
+            type: 'Cat Bathroom',
+            description: 'Bathroom for Cats',
+            completion_date: '2018-12-25'
+          },
+          financial: {
+            date: '2017-12-25',
+            funded_through_HIF: true
+          }
+        }
+      },
+      {
+        project_id: 1,
+        data:
+        {
+          summary: {
+            project_name: 'Cats Embassy',
+            description: 'Embassy for cats in the UK',
+            lead_authority: 'Made Tech'
+          },
+          infrastructure: {
+            type: 'Cat waiting room',
+            description: 'A waiting room for cats',
+            completion_date: '2019-09-01'
+          },
+          financial: {
+            date: '2019-09-01',
+            funded_through_HIF: true
+          }
+        }
+      }
+
+    ]
+  }
+  it 'can get multiple returns by project id from a gateway' do
+    expect(get_use_case(:create_returns)).execute(project_id: 1, data:
       {
         summary: {
           project_name: 'Cats Protection League',
@@ -23,11 +66,26 @@ describe 'Getting multiple returns' do
           date: '2017-12-25',
           funded_through_HIF: true
         }
-      }
-    },
-    {
-      project_id: 2,
-      data:
+      })
+
+    expect(get_use_case(:create_returns)).execute(project_id: 1, data:
+      {
+        summary: {
+          project_name: 'Cats Embassy',
+          description: 'Embassy for cats in the UK',
+          lead_authority: 'Made Tech'
+        },
+        infrastructure: {
+          type: 'Cat waiting room',
+          description: 'A waiting room for cats',
+          completion_date: '2019-09-01'
+        },
+        financial: {
+          date: '2019-09-01',
+          funded_through_HIF: true
+        }
+      })
+    expect(get_use_case(:create_returns)).execute(project_id: 2, data:
       {
         summary: {
           project_name: 'Dog Protection League',
@@ -43,127 +101,8 @@ describe 'Getting multiple returns' do
           date: '2019-12-06',
           funded_through_HIF: true
         }
-      }
-    },
-    {
-      project_id: 1,
-      data:
-      {
-        summary: {
-          project_name: 'Cats Embassy',
-          description: 'Embassy for cats in the UK',
-          lead_authority: 'Made Tech'
-        },
-        infrastructure: {
-          type: 'Cat waiting room',
-          description: 'A waiting room for cats',
-          completion_date: '2019-09-01'
-        },
-        financial: {
-          date: '2019-09-01',
-          funded_through_HIF: true
-        }
-      }
-    }
-    ]}
+      })
 
-
-  let(:returns_for_project_1) {[
-    {
-      project_id: 1,
-      data:
-      {
-        summary: {
-          project_name: 'Cats Protection League',
-          description: 'A new headquarters for all the Cats',
-          lead_authority: 'Made Tech'
-        },
-        infrastructure: {
-          type: 'Cat Bathroom',
-          description: 'Bathroom for Cats',
-          completion_date: '2018-12-25'
-        },
-        financial: {
-          date: '2017-12-25',
-          funded_through_HIF: true
-        }
-      }
-    },
-    {
-      project_id: 1,
-      data:
-      {
-        summary: {
-          project_name: 'Cats Embassy',
-          description: 'Embassy for cats in the UK',
-          lead_authority: 'Made Tech'
-        },
-        infrastructure: {
-          type: 'Cat waiting room',
-          description: 'A waiting room for cats',
-          completion_date: '2019-09-01'
-        },
-        financial: {
-          date: '2019-09-01',
-          funded_through_HIF: true
-        }
-      }
-    }
-    ]}
-
-  let(:return_objects_for_project_1) {[
-    double(
-      project_id: 1,
-      data:
-      {
-        summary: {
-          project_name: 'Cats Protection League',
-          description: 'A new headquarters for all the Cats',
-          lead_authority: 'Made Tech'
-        },
-        infrastructure: {
-          type: 'Cat Bathroom',
-          description: 'Bathroom for Cats',
-          completion_date: '2018-12-25'
-        },
-        financial: {
-          date: '2017-12-25',
-          funded_through_HIF: true
-        }
-      }
-    ),
-    double(
-      project_id: 1,
-      data:
-      {
-        summary: {
-          project_name: 'Cats Embassy',
-          description: 'Embassy for cats in the UK',
-          lead_authority: 'Made Tech'
-        },
-        infrastructure: {
-          type: 'Cat waiting room',
-          description: 'A waiting room for cats',
-          completion_date: '2019-09-01'
-        },
-        financial: {
-          date: '2019-09-01',
-          funded_through_HIF: true
-        }
-      }
-    )
-    ]}
-
-  let(:return_gateway) { spy(get_returns: return_objects_for_project_1)}
-
-  before do
-      stub_const(
-        'LocalAuthority::Gateway::ReturnGateway',
-        double(new: return_gateway)
-      )
-  end
-
-  it 'can get multiple returns by project id from a gateway' do
     expect(get_use_case(:get_returns).execute(project_id: 1)).to eq(returns_for_project_1)
   end
 end
