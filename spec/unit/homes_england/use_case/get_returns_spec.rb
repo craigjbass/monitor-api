dbc677c24ce1168b521e1acb8b7929e3d28ab0fd
faissalMT <faissal@madetech.com>
2018-07-30 11:10:34 +0100

    Testing communication with gateway

--- spec/unit/homes_england/use_case/get_returns_spec.rb
+++ spec/unit/homes_england/use_case/get_returns_spec.rb
@@ -2,29 +2,7 @@ require 'rspec'
 
 describe HomesEngland::UseCase::GetReturns, focus: true do
 
-  let(:all_returns) {[
-    {
-      project_id: 1,
-      data:
-      {
-        summary: {
-          project_name: 'Cats Protection League',
-          description: 'A new headquarters for all the Cats',
-          lead_authority: 'Made Tech'
-        },
-        infrastructure: {
-          type: 'Cat Bathroom',
-          description: 'Bathroom for Cats',
-          completion_date: '2018-12-25'
-        },
-        financial: {
-          date: '2017-12-25',
-          funded_through_HIF: true
-        }
-      }
-    }
-    ]}
-
+  let(:all_returns) { [] }
   let(:return_gateway) { spy(get_returns: all_returns)}
 
   let(:get_returns) {described_class.new(return_gateway: return_gateway)}
@@ -35,8 +13,37 @@ describe HomesEngland::UseCase::GetReturns, focus: true do
         double(new: return_gateway)
       )
   end
-  
-  it 'can get a single return as an item in an array' do
-    expect(get_returns.execute(project_id: 1)).to eq(all_returns)
+
+  context 'with a single item' do
+    let(:all_returns) {[
+      {
+        project_id: 1,
+        data:
+        {
+          summary: {
+            project_name: 'Cats Protection League',
+            description: 'A new headquarters for all the Cats',
+            lead_authority: 'Made Tech'
+          },
+          infrastructure: {
+            type: 'Cat Bathroom',
+            description: 'Bathroom for Cats',
+            completion_date: '2018-12-25'
+          },
+          financial: {
+            date: '2017-12-25',
+            funded_through_HIF: true
+          }
+        }
+      }]}
+
+    it 'sends the correct project_id to return_gateway' do
+      get_returns.execute(project_id: 1)
+      expect(return_gateway).to have_received(:get_returns).with(project_id: 1)
+    end
+
+    it 'can get a single return as an item in an array' do
+      expect(get_returns.execute(project_id: 1)).to eq(all_returns)
+    end
   end
 end
