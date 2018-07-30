5b7d40a3ac1254982523fb41215e826a9674af7e
faissalMT <faissal@madetech.com>
2018-07-30 13:51:47 +0100

    Cleaner get_returns

--- lib/local_authority/gateway/sequel_return.rb
+++ lib/local_authority/gateway/sequel_return.rb
@@ -19,13 +19,11 @@ class LocalAuthority::Gateway::SequelReturn
   end
 
   def get_returns(project_id:)
-    returns_array = []
-    @database[:returns].where(project_id: project_id).each do |db_r|
-      returns_array << LocalAuthority::Domain::Return.new.tap do |r|
+    @database[:returns].where(project_id: project_id).all.map do |db_r|
+      LocalAuthority::Domain::Return.new.tap do |r|
         r.project_id = db_r[:project_id]
         r.data = Common::DeepSymbolizeKeys.to_symbolized_hash(db_r[:data].to_h)
       end
     end
-    returns_array
   end
 end
