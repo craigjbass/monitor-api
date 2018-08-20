df8ba7caadf6562905419ab3f5f00ab916ebec78
Robin Lacey <robin@madetech.com>
2018-08-20 21:00:53 +0100

    added space to send notification

--- lib/local_authority/use_case/send_notification.rb
+++ lib/local_authority/use_case/send_notification.rb
@@ -6,6 +6,6 @@ class LocalAuthority::UseCase::SendNotification
   end
 
   def execute(to:, url:, access_token:)
-    @notification_gateway.send_notification(to: to, url: url,access_token: access_token)
+    @notification_gateway.send_notification(to: to, url: url, access_token: access_token)
   end
 end
