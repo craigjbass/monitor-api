339d04f9a5fe2a06f5279e96b031d8d1d6892666
faissalMT <faissal@madetech.com>
2018-09-06 10:30:43 +0100

    Cleanup ValidateReturn use case

--- lib/local_authority/use_case/validate_return.rb
+++ lib/local_authority/use_case/validate_return.rb
@@ -14,30 +14,55 @@ class LocalAuthority::UseCase::ValidateReturn
       invalid_paths: invalid_paths }
   end
 
+  private
+
+  def message_get_json_properties(message)
+    ignore_json_property_root = %r{#/}
+    capture_json_property = %r{([\w/]*)}
+    ignore_until = /.*/
+    message_parse_regex = /
+      '
+        #{ignore_json_property_root}
+        #{capture_json_property}
+      '
+      #{ignore_until}
+      '
+        #{capture_json_property}
+      '
+    /x
+    message.match(message_parse_regex)&.captures&.to_a&.reject(&:empty?)
+  end
+
+  def json_property_name_to_path(property)
+    property.split('/').map do |node|
+      begin
+        Integer(node)
+      rescue ArgumentError
+        node.to_sym
+      end
+    end
+  end
+
+  def join_path_array(array_of_paths)
+    array_of_paths.flatten
+  end
+
+  def json_properties_to_path(properties)
+    json_paths = properties.map do |property|
+      json_property_name_to_path(property)
+    end
+    join_path_array(json_paths)
+  end
+
   def get_paths_from_error_messages(schema, return_data)
-    fully_validate = JSON::Validator.fully_validate(schema.schema.to_json, return_data)
-    fully_validate.map do |message|
-      json_property_name_regex_capture = %r{([\w\/]*)}
-      message_parse_regex = %r{
-        '\#/
-          #{json_property_name_regex_capture}
-        '
-        .*
-        '
-          #{json_property_name_regex_capture}
-        '
-      }x
-      message.scan(message_parse_regex).map do |match|
-        match.reject(&:empty?).map do |path|
-          path.split('/').map do |node|
-            begin
-              Integer(node)
-            rescue ArgumentError
-              node.to_sym
-            end
-          end
-        end.flatten
-      end.first
+    validation_messages = JSON::Validator.fully_validate(
+      schema.schema.to_json,
+      return_data
+    )
+
+    validation_messages.map do |message|
+      properties = message_get_json_properties(message)
+      json_properties_to_path(properties) unless properties.nil?
     end.compact
   end
 end
