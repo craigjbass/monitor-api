f22b90b329b2f08fb73ce49a19431cfe3ca43d3c
Daniel Burnley <dan@danops.uk>
2018-09-12 15:00:04 +0100

    Expire tokens 4 hours after they are generated

--- lib/local_authority/use_case/create_api_key.rb
+++ lib/local_authority/use_case/create_api_key.rb
@@ -1,6 +1,9 @@
+require 'date'
+
 class LocalAuthority::UseCase::CreateApiKey
   def execute(project_id:)
-    api_key = JWT.encode({project_id: project_id}, ENV['HMAC_SECRET'], 'HS512')
+    exp = DateTime.now.strftime("%s").to_i + (60 * 4)
+    api_key = JWT.encode({project_id: project_id, exp: exp}, ENV['HMAC_SECRET'], 'HS512')
     
     { api_key: api_key }
   end
