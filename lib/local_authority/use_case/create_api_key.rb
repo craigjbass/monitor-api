8e69efdcb04a3cc7e9d5596863e55e23daaa5b11
Daniel Burnley <dan@danops.uk>
2018-09-13 11:17:59 +0100

    Refactor token generation to be clearer on numbers

--- lib/local_authority/use_case/create_api_key.rb
+++ lib/local_authority/use_case/create_api_key.rb
@@ -2,9 +2,21 @@ require 'date'
 
 class LocalAuthority::UseCase::CreateApiKey
   def execute(project_id:)
-    exp = DateTime.now.strftime("%s").to_i + (60 * 4)
-    api_key = JWT.encode({project_id: project_id, exp: exp}, ENV['HMAC_SECRET'], 'HS512')
-    
+    api_key = JWT.encode(
+      { project_id: project_id, exp: four_hours_from_now },
+      ENV['HMAC_SECRET'],
+      'HS512'
+    )
+
     { api_key: api_key }
+  end
+
+  private
+
+  def four_hours_from_now
+    current_time_in_seconds = DateTime.now.strftime('%s').to_i
+    four_hours_in_seconds = 60 * 4
+
+    current_time_in_seconds + four_hours_in_seconds
   end
 end
