0e556e4ee10d36d1ee1dd35d0980269d33fc8ae2
Craig J. Bass <craig@madetech.co.uk>
2018-09-13 15:24:05 +0100

    Extract hash deep merge

--- lib/local_authority/namespaces.rb
+++ lib/local_authority/namespaces.rb
@@ -2,4 +2,5 @@ module LocalAuthority
   module UseCase; end
   module Gateway; end
   module Domain; end
+  module Refinement; end
 end
--- lib/local_authority/use_case/calculate_hif_return.rb
+++ lib/local_authority/use_case/calculate_hif_return.rb
@@ -2,13 +2,14 @@
 require 'date'
 
 class LocalAuthority::UseCase::CalculateHIFReturn
+  using LocalAuthority::Refinement::HashDeepMerge
+
   def execute(return_data_with_no_calculations:, previous_return:)
     new_return_data = return_data_with_no_calculations
     new_return_data[:infrastructures] = [] if new_return_data[:infrastructures].nil?
 
     new_return_data[:infrastructures].each_with_index do |_value, i|
-      new_return_data[:infrastructures][i] = deep_merge(
-        new_return_data[:infrastructures][i],
+      new_return_data[:infrastructures][i] = new_return_data[:infrastructures][i].deep_merge(
         {
           planning: {
             planningNotGranted: {
@@ -24,9 +25,8 @@ class LocalAuthority::UseCase::CalculateHIFReturn
         }
       )
 
-      last_date = get_currentReturn(previous_return, i)
-      current_date = get_currentReturn(new_return_data, i)
-
+      last_date = current_return(previous_return, i)
+      current_date = current_return(new_return_data, i)
 
       update_variance_last_return_full_planning_permission_submitted(
         new_return_data,
@@ -45,7 +45,7 @@ class LocalAuthority::UseCase::CalculateHIFReturn
 
   private
 
-  def get_currentReturn(return_data, index)
+  def current_return(return_data, index)
     return_data&.dig(:infrastructures,
       index,
       :planning,
@@ -72,21 +72,5 @@ class LocalAuthority::UseCase::CalculateHIFReturn
     date_two = Date.parse date_as_string_two
 
     (date_one.cweek - date_two.cweek).to_s
-  end
-
-  # based on : https://stackoverflow.com/a/30225093
-  def deep_merge(first, second)
-    merger = proc do |_key, v1, v2|
-      if v1.class == Hash && v2.class == Hash
-        v1.merge(v2, &merger)
-      else
-        if v1.class == Array && v2.class == Array
-          v1 | v2
-        else
-          [:undefined, nil, :nil].include?(v2) ? v1 : v2
-        end
-      end
-    end
-    first.merge(second.to_h, &merger)
   end
 end
