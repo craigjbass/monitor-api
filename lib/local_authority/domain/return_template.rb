c699d497b617025e5069241bfac63c3c62b613c3
Craig J. Bass <craig@madetech.co.uk>
2018-09-13 10:32:15 +0100

    Extract ValidationMessage class

--- lib/local_authority/domain/return_template.rb
+++ lib/local_authority/domain/return_template.rb
@@ -1,11 +1,35 @@
 class LocalAuthority::Domain::ReturnTemplate
   attr_accessor :layout, :schema
 
+  class ValidationMessage
+    def initialize(message)
+      @message = message
+    end
+
+    def json_properties
+      ignore_json_property_root = %r{#/}
+      capture_json_property = %r{([\w/]*)}
+      ignore_until = /.*/
+      message_parse_regex = /
+        '
+          #{ignore_json_property_root}
+        #{capture_json_property}
+        '
+        #{ignore_until}
+        '
+          #{capture_json_property}
+        '
+      /x
+
+      @message.match(message_parse_regex)&.captures&.to_a&.reject(&:empty?)
+    end
+  end
+
   def invalid_paths(the_return)
     messages = validation_messages_for(the_return)
 
     paths = messages.map do |m|
-      json_properties = json_properties_for(m)
+      json_properties = m.json_properties
       path_for(json_properties) unless json_properties.nil?
     end.compact
 
@@ -15,27 +39,12 @@ class LocalAuthority::Domain::ReturnTemplate
   private
 
   def validation_messages_for(return_data)
-    JSON::Validator.fully_validate(
+    messages = JSON::Validator.fully_validate(
       schema.to_json,
       return_data
     )
-  end
 
-  def json_properties_for(message)
-    ignore_json_property_root = %r{#/}
-    capture_json_property = %r{([\w/]*)}
-    ignore_until = /.*/
-    message_parse_regex = /
-      '
-        #{ignore_json_property_root}
-    #{capture_json_property}
-      '
-      #{ignore_until}
-      '
-        #{capture_json_property}
-      '
-    /x
-    message.match(message_parse_regex)&.captures&.to_a&.reject(&:empty?)
+    messages.map { |m| ValidationMessage.new(m) }
   end
 
   def json_property_name_to_path(property)
