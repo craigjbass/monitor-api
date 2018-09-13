2a6bba1ee90db17b4e84943695271716e285726c
Craig J. Bass <craig@madetech.co.uk>
2018-09-13 11:18:11 +0100

    Extract a method
    So we can remove a comment

--- lib/local_authority/use_case/send_notification.rb
+++ lib/local_authority/use_case/send_notification.rb
@@ -6,7 +6,17 @@ class LocalAuthority::UseCase::SendNotification
   end
 
   def execute(to:, url:, access_token:)
-    url_to_send = url.gsub(/\/?\?.*/, '') #Removes URL params and forward slash prefix if present
-    @notification_gateway.send_notification(to: to, url: url_to_send, access_token: access_token)
+    url_to_send = remove_trailing_slash_and_query_parameters(url)
+    @notification_gateway.send_notification(
+      to: to,
+      url: url_to_send,
+      access_token: access_token
+    )
+  end
+
+  private
+
+  def remove_trailing_slash_and_query_parameters(url)
+    url.gsub(/\/?\?.*/, '')
   end
 end
