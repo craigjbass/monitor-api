3323df1ad65178f80979d678e813be983e6c84d1
faissalMT <faissal@madetech.com>
2018-09-14 12:09:23 +0100

    Refactor and support multiple entries in OneOf (populate_return_template)

--- lib/local_authority/use_case/populate_return_template.rb
+++ lib/local_authority/use_case/populate_return_template.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class LocalAuthority::UseCase::PopulateReturnTemplate
   def initialize(template_gateway:, find_path_data:, get_schema_copy_paths:)
     @template_gateway = template_gateway
@@ -25,10 +27,6 @@ class LocalAuthority::UseCase::PopulateReturnTemplate
 
   private
 
-  def create_root(baseline_data, return_data)
-    { baseline_data: baseline_data, return_data: return_data }
-  end
-
   def copy_data(copy_path_pair, source_data, template_schema, return_data)
     path_types = schema_types(template_schema, copy_path_pair[:to]).drop(1)
 
@@ -43,26 +41,6 @@ class LocalAuthority::UseCase::PopulateReturnTemplate
       path_types,
       found_data
     )
-  end
-
-  def last_node?(path)
-    path.empty?
-  end
-
-  def node_is_array?(path_types)
-    path_types.first == :array
-  end
-
-  def node_is_object?(path_types)
-    path_types.first == :object
-  end
-
-  def ensure_hash_exists(hash, path)
-    hash[path.first] = {} if hash[path.first].nil?
-  end
-
-  def ensure_array_exists(hash, path)
-    hash[path.first] = [] if hash[path.first].nil?
   end
 
   def descend_array(hash, path, path_types, value)
@@ -85,9 +63,9 @@ class LocalAuthority::UseCase::PopulateReturnTemplate
     )
   end
 
-  #Burying is when we descend through a hash and look for our path where we place
-  #our value if path_types contains arrays descend_array and bury will split up
-  #the array of values that will exist
+  # Burying is when we descend through a hash and look for our path where we place
+  # our value if path_types contains arrays descend_array and bury will split up
+  # the array of values that will exist
   def descend_hash_and_bury(hash, path, path_types, value)
     if last_node?(path)
       value
@@ -106,62 +84,120 @@ class LocalAuthority::UseCase::PopulateReturnTemplate
   def descend_array_and_bury(array, path, path_types, value)
     array = [] if array.nil?
     value.zip(array).map do |to_put, hash|
-
       hash = {} if hash.nil?
       descend_hash_and_bury(hash, path, path_types, to_put)
     end
   end
 
-  def path_not_found?(path)
-    path[-1] == :not_found
+  def schema_types(schema, path)
+    return [:not_found] if schema.nil?
+    return [:object] if path.empty?
+    return get_schema_types_from_array(path, schema) if schema[:type] == 'array'
+    return get_schema_types_from_object(path, schema) if schema[:type] == 'object'
+    [:unsupported_type]
+  end
+
+  def get_schema_types_from_array(path, schema)
+    acquired_path = [:not_found]
+    if array_has_property?(path, schema)
+      acquired_path = get_path_types_from_array_properties(path, schema)
+    end
+    if array_has_item_dependencies?(schema) && path_not_found?(acquired_path)
+      acquired_path = get_path_types_from_array_last_dependency(path, schema)
+    end
+    acquired_path
+  end
+
+  def get_schema_types_from_object(path, schema)
+    acquired_path = [:not_found]
+
+    if object_has_property(path, schema)
+      acquired_path = get_path_types_from_object_properties(path, schema)
+    end
+
+    if object_has_dependencies(schema) && path_not_found?(acquired_path)
+      acquired_path = get_path_types_object_last_dependency(path, schema)
+    end
+
+    [:object] + acquired_path
+  end
+
+  def create_root(baseline_data, return_data)
+    { baseline_data: baseline_data, return_data: return_data }
+  end
+
+  def last_node?(path)
+    path.empty?
+  end
+
+  def node_is_array?(path_types)
+    path_types.first == :array
+  end
+
+  def node_is_object?(path_types)
+    path_types.first == :object
+  end
+
+  def ensure_hash_exists(hash, path)
+    hash[path.first] = {} if hash[path.first].nil?
+  end
+
+  def ensure_array_exists(hash, path)
+    hash[path.first] = [] if hash[path.first].nil?
   end
 
   def hash_has_path(hash, path)
     hash.dig(*path).nil? != true
   end
 
-  def schema_types(schema, path)
-    if schema.nil?
-      [:not_found]
-    elsif path.empty?
-      [:object]
-    elsif schema[:type] == 'array'
-      acquired_path = [:not_found]
+  def path_not_found?(path)
+    path.last == :not_found
+  end
 
-      if hash_has_path(schema, [:items, :properties, path[0]])
-        acquired_path = [:array] + schema_types(schema.dig(:items, :properties, path[0]), path.drop(1))
-      end
+  def path_found?(path)
+    path.last != :not_found
+  end
 
-      if hash_has_path(schema, [:items, :dependencies]) && path_not_found?(acquired_path)
-        schema.dig(:items, :dependencies).values.each do |value|
-          acquired_path = [:array] + browse_possible_states(value, path)
-        end
-      end
-      acquired_path
+  def get_path_types_from_array_last_dependency(path, schema)
+    [:array] + get_last_state_path(schema.dig(:items, :dependencies).values.last, path)
+  end
 
-    elsif schema[:type] == 'object'
-      acquired_path = [:not_found]
+  def array_has_item_dependencies?(schema)
+    hash_has_path(schema, %i[items dependencies])
+  end
 
-      if hash_has_path(schema, [:properties, path[0]])
-        acquired_path = schema_types(schema[:properties][path[0]], path.drop(1))
-      end
+  def get_path_types_from_array_properties(path, schema)
+    [:array] + schema_types(schema.dig(:items, :properties, path.first), path.drop(1))
+  end
 
-      if hash_has_path(schema, [:dependencies]) && path_not_found?(acquired_path)
-        schema[:dependencies].values.each do |value|
-          acquired_path = browse_possible_states(value, path)
-        end
-      end
+  def array_has_property?(path, schema)
+    hash_has_path(schema, [:items, :properties, path.first])
+  end
 
-      if acquired_path.nil?
-        [:object]
-      else
-        [:object] + acquired_path
-      end
+  def get_path_types_object_last_dependency(path, schema)
+    get_last_state_path(schema[:dependencies].values.last, path)
+  end
+
+  def get_path_types_from_object_properties(path, schema)
+    schema_types(schema[:properties][path.first], path.drop(1))
+  end
+
+  def object_has_dependencies(schema)
+    hash_has_path(schema, [:dependencies])
+  end
+
+  def object_has_property(path, schema)
+    hash_has_path(schema, [:properties, path.first])
+  end
+
+  def get_last_state_path(schema, path)
+    schema[:oneOf].each do |value|
+      path = get_path_from_object(value,path)
+      return path if path_found?(path)
     end
   end
 
-  def browse_possible_states(schema, path)
-    # This will break if the dependency we're searching for is in anything other than the last
-    schema_types(schema[:oneOf].last.dig(:properties, path[0]), path.drop(1))
+  def get_path_from_object(value, path)
+    schema_types(value.dig(:properties, path.first), path.drop(1))
   end
 end
