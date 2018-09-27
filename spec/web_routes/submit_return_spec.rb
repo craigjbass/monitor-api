61478aec712bfd97321305faddc4c25d8b39b679
Daniel Burnley <dan@danops.uk>
2018-09-27 09:21:09 +0100

    Add guard to endpoints

--- spec/web_routes/submit_return_spec.rb
+++ spec/web_routes/submit_return_spec.rb
@@ -9,21 +9,26 @@ describe 'Submitting a return' do
       'LocalAuthority::UseCase::SubmitReturn',
       double(new: submit_return_spy)
     )
+
+    stub_const(
+      'LocalAuthority::UseCase::CheckApiKey',
+      double(new: double(execute: {valid: true}))
+    )
   end
 
   it 'submitting nothing should return 400' do
-    post '/return/submit', nil
+    post '/return/submit', nil, 'HTTP_API_KEY' => 'superSecret'
     expect(last_response.status).to eq(400)
   end
 
   it 'submitting a valid id should return 200' do
-    post '/return/submit', {return_id: 1}.to_json
+    post '/return/submit', {return_id: 1}.to_json, 'HTTP_API_KEY' => 'superSecret'
     expect(last_response.status).to eq(200)
   end
 
   context 'example one' do
     it 'will run submit return use case with id' do
-      post '/return/submit', {return_id: 1}.to_json
+      post '/return/submit', {return_id: 1}.to_json, 'HTTP_API_KEY' => 'superSecret'
       expect(submit_return_spy).to have_received(:execute).with(return_id: 1)
     end
   end
@@ -31,9 +36,8 @@ describe 'Submitting a return' do
 
   context 'example two' do
     it 'will run submit return use case with id' do
-      post '/return/submit', {return_id: 42}.to_json
+      post '/return/submit', {return_id: 42}.to_json, 'HTTP_API_KEY' => 'superSecret'
       expect(submit_return_spy).to have_received(:execute).with(return_id: 42)
     end
   end
-
 end
