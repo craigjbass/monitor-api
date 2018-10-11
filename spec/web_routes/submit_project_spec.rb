060dc0f7269399bef3e465b474128d92f2d56eac
Emma Corbett <emma@madetech.com>
2018-10-11 10:03:13 +0100

    Add test for when API key is invalid

--- spec/web_routes/submit_project_spec.rb
+++ spec/web_routes/submit_project_spec.rb
@@ -14,31 +14,45 @@ describe 'Submitting a project' do
 
     stub_const(
       'LocalAuthority::UseCase::CheckApiKey',
-      double(new: double(execute: { valid: true }))
+      double(new: check_api_key_spy)
     )
   end
+ 
+  context 'If API key is invalid' do
+    let(:check_api_key_spy) { spy(execute: { valid: false })}
 
-  it 'returns 400 when submitting nothing' do
-    post 'project/submit', nil, 'HTTP_API_KEY' => 'superSecret'
-    expect(last_response.status).to eq(400)
-  end
+    it 'returns 401' do
+      post 'project/submit', { project_id: '1' }.to_json, 'HTTP_API_KEY' => 'notSoSecret'
 
-  it 'returns 200 when submitting a valid id' do
-    post 'project/submit', { project_id: '1' }.to_json, 'HTTP_API_KEY' => 'superSecret'
-    expect(last_response.status).to eq(200)
-  end
-
-  context 'example one' do
-    it 'will run submit project use case with id' do
-      post '/project/submit', { project_id: '1' }.to_json, 'HTTP_API_KEY' => 'superSecret'
-      expect(submit_project_spy).to have_received(:execute).with(project_id: 1)
+      expect(last_response.status).to eq(401)
     end
   end
 
-  context 'example two' do
-    it 'will run submit project use case with id' do
-      post '/project/submit', { project_id: '5' }.to_json, 'HTTP_API_KEY' => 'superSecret'
-      expect(submit_project_spy).to have_received(:execute).with(project_id: 5)
+  context 'If API key is valid' do
+    let(:check_api_key_spy) { spy(execute: { valid: true })}
+
+    it 'returns 400 when submitting nothing' do
+      post 'project/submit', nil, 'HTTP_API_KEY' => 'superSecret'
+      expect(last_response.status).to eq(400)
+    end
+
+    it 'returns 200 when submitting a valid id' do
+      post 'project/submit', { project_id: '1' }.to_json, 'HTTP_API_KEY' => 'superSecret'
+      expect(last_response.status).to eq(200)
+    end
+
+    context 'example one' do
+      it 'will run submit project use case with id' do
+        post '/project/submit', { project_id: '1' }.to_json, 'HTTP_API_KEY' => 'superSecret'
+        expect(submit_project_spy).to have_received(:execute).with(project_id: 1)
+      end
+    end
+
+    context 'example two' do
+      it 'will run submit project use case with id' do
+        post '/project/submit', { project_id: '5' }.to_json, 'HTTP_API_KEY' => 'superSecret'
+        expect(submit_project_spy).to have_received(:execute).with(project_id: 5)
+      end
     end
   end
 end
