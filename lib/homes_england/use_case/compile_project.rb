8478e8fd86ea129c3d4e48799c6a58f3a3ecc28c
Faissal Bensefia <faissal@madetech.com>
2018-10-19 09:50:36 +0100

    Refactor CompileProject

--- lib/homes_england/use_case/compile_project.rb
+++ lib/homes_england/use_case/compile_project.rb
@@ -5,24 +5,36 @@ class HomesEngland::UseCase::CompileProject
   end
 
   def execute(project_id:)
-    baseline = @find_project.execute(id: project_id)
-    baseline[:project_id] = project_id
-    baseline.delete(:status)
+    baseline_data = @find_project.execute(id: project_id)
+    new_baseline = format_baseline_data(baseline_data, project_id)
 
     submitted_returns = @get_returns.execute(project_id: project_id)[:returns]
     unless submitted_returns.empty?
-      submitted_returns.each do |i|
-        i[:data] = i.dig(:updates, -1)
-        i.delete(:updates)
-        i.delete(:status)
+      new_submitted_returns = submitted_returns.map do |return_data|
+        format_return_data(return_data)
       end
     end
 
     {
       compiled_project: {
-        baseline: baseline,
-        submitted_returns: submitted_returns
+        baseline: new_baseline,
+        submitted_returns: new_submitted_returns
       }
     }
+  end
+
+  def format_baseline_data(baseline_data, project_id)
+    new_baseline_data = baseline_data.dup
+    new_baseline_data[:project_id] = project_id
+    new_baseline_data.delete(:status)
+    new_baseline_data
+  end
+
+  def format_return_data(return_data)
+    new_return_data = return_data.dup
+    new_return_data[:data] = new_return_data.dig(:updates, -1)
+    new_return_data.delete(:updates)
+    new_return_data.delete(:status)
+    new_return_data
   end
 end
