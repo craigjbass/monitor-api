99fa6d63cb548d5c6a6157606f6b4ba99a7bd33a
Christopher Parsons <mail@christopherparsons.me>
2018-10-23 11:32:03 +0100

    Add test for LA Draft status to submit_project_spec

--- spec/unit/homes_england/use_case/submit_project_spec.rb
+++ spec/unit/homes_england/use_case/submit_project_spec.rb
@@ -1,20 +1,41 @@
 # frozen_string_literal: true
 
 describe HomesEngland::UseCase::SubmitProject do
-  let(:project_gateway) { spy(status: 'Draft') }
-  let(:use_case) { described_class.new(project_gateway: project_gateway) }
+  describe 'When status is Draft' do
+    let(:project_gateway) { spy(status: 'Draft') }
+    let(:use_case) { described_class.new(project_gateway: project_gateway) }
 
-  context 'Example 1' do
-    it 'calls the submit method' do
-      use_case.execute(project_id: 1)
-      expect(project_gateway).to have_received(:submit).with(id: 1, status: 'LA Draft')
+    context 'Example 1' do
+      it 'calls the submit method' do
+        use_case.execute(project_id: 1)
+        expect(project_gateway).to have_received(:submit).with(id: 1, status: 'LA Draft')
+      end
+    end
+
+    context 'Example 2' do
+      it 'calls the submit method' do
+        use_case.execute(project_id: 7)
+        expect(project_gateway).to have_received(:submit).with(id: 7, status: 'LA Draft')
+      end
     end
   end
 
-  context 'Example 2' do
-    it 'calls the submit method' do
-      use_case.execute(project_id: 7)
-      expect(project_gateway).to have_received(:submit).with(id: 7, status: 'LA Draft')
+  describe 'When status is LA Draft' do
+    let(:project_gateway) { spy(status: 'LA Draft') }
+    let(:use_case) { described_class.new(project_gateway: project_gateway) }
+
+    context 'Example 1' do
+      it 'calls the submit method' do
+        use_case.execute(project_id: 1)
+        expect(project_gateway).to have_received(:submit).with(id: 1, status: 'Submitted')
+      end
+    end
+
+    context 'Example 2' do
+      it 'calls the submit method' do
+        use_case.execute(project_id: 7)
+        expect(project_gateway).to have_received(:submit).with(id: 7, status: 'Submitted')
+      end
     end
   end
 end
