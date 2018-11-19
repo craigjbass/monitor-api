e1b71208c92728fbb655fceb795429e61b5586ac
Emma Corbett <emma@madetech.com>
2018-11-19 16:50:08 +0000

    Remove unnecessary dependencies

--- spec/acceptance/ui/hif_validate_return_spec.rb
+++ spec/acceptance/ui/hif_validate_return_spec.rb
@@ -27,4 +27,196 @@ describe 'Validates HIF return' do
       ])
     end
   end
+
+
+  context 'required field validation' do
+    let(:template) do
+      Common::Domain::Template.new.tap do |p|
+        p.schema = {
+          fundingPackages: {
+            type: 'array',
+            title: 'Funding Packages',
+            items: {
+              type: 'object',
+              title: 'Funding for Infrastructure',
+              properties: {
+                fundingStack: {
+                  type: 'object',
+                  title: 'Funding stack',
+                  properties: {
+                    hifSpend: {
+                      title: 'HIF Spend',
+                      type: 'object',
+                      horizontal: true,
+                      properties: {
+                        baseline: {
+                          type: 'string',
+                          title: 'HIF Baseline Amount',
+                          sourceKey: %i[baseline_data costs infrastructure HIFAmount],
+                          readonly: true,
+                          currency: true
+                        },
+                        current: {
+                          type: 'string',
+                          title: 'Current Return',
+                          currency: true
+                        },
+                        lastReturn: {
+                          type: 'string',
+                          title: 'Last Return',
+                          readonly: true,
+                          currency: true,
+                          sourceKey: %i[return_data fundingPackages hifSpend current]
+                        }
+                      }
+                    },
+                    totalCost: {
+                      title: 'Total Cost',
+                      type: 'object',
+                      horizontal: true,
+                      properties: {
+                        baseline: {
+                          type: 'string',
+                          title: 'Baseline Amount',
+                          sourceKey: %i[baseline_data costs infrastructure totalCostOfInfrastructure],
+                          readonly: true,
+                          currency: true
+                        },
+                        current: {
+                          type: 'string',
+                          title: 'Current return',
+                          currency: true
+                        },
+                        varianceReason: {
+                          type: 'string',
+                          title: 'Reason for variance'
+                        },
+                        percentComplete: {
+                          type: 'string',
+                          title: 'Percent complete',
+                          percentage: true
+                        }
+                      }
+                    },
+                    fundedThroughHIF: {
+                      type: 'string',
+                      title: 'Totally funded through HIF?',
+                      radio: true,
+                      enum: %w[Yes No],
+                      readonly: true,
+                      sourceKey: %i[baseline_data costs infrastructure totallyFundedThroughHIF]
+                    }
+                  },
+                  dependencies: {
+                    fundedThroughHIF: {
+                      oneOf: [
+                        {
+                          properties: {
+                            fundedThroughHIF: {
+                              enum: ['Yes']
+                            }
+                          }
+                        },
+                        {
+                          properties: {
+                            fundedThroughHIF: {
+                              enum: ['No']
+                            },
+                            descriptionOfFundingStack: {
+                              type: 'string',
+                              title: 'Description of funding stack',
+                              readonly: true,
+                              sourceKey: %i[baseline_data costs infrastructure descriptionOfFundingStack]
+                            },
+                            riskToFundingPackage: {
+                              type: 'object',
+                              horizontal: true,
+                              title: 'Risk to funding package',
+                              properties: {
+                                risk: {
+                                  title: 'Is there a risk?',
+                                  type: 'string',
+                                  radio: true,
+                                  enum: %w[Yes No]
+                                },
+                                description: {
+                                  title: 'Description of risk',
+                                  type: 'string'
+                                }
+                              }
+                            },
+                            public: {
+                              title: 'Total Public',
+                              type: 'object',
+                              horizontal: true,
+                              properties: {
+                                baseline: {
+                                  title: 'Total baseline amount',
+                                  type: 'string',
+                                  readonly: true,
+                                  currency: true,
+                                  sourceKey: %i[baseline_data costs infrastructure totalPublic]
+                                },
+                                current: {
+                                  title: 'Total - Current return',
+                                  type: 'string',
+                                  currency: true
+                                },
+                                reason: {
+                                  title: 'Reason for variance',
+                                  type: 'string'
+                                },
+                                amountSecured: {
+                                  title: 'Amount secured to date',
+                                  type: 'string',
+                                  currency: true
+                                }
+                              }
+                            },
+                            private: {
+                              title: 'Total Private',
+                              type: 'object',
+                              horizontal: true,
+                              properties: {
+                                baseline: {
+                                  title: 'Total baseline amount',
+                                  type: 'string',
+                                  readonly: true,
+                                  currency: true,
+                                  sourceKey: %i[baseline_data costs infrastructure totalPrivate]
+                                },
+                                current: {
+                                  title: 'Total - Current return',
+                                  type: 'string',
+                                  currency: true
+                                },
+                                reason: {
+                                  title: 'Reason for variance',
+                                  type: 'string'
+                                },
+                                amountSecured: {
+                                  title: 'Amount secured to date',
+                                  type: 'string',
+                                  currency: true
+                                }
+                              }
+                            }
+                          }
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      end
+    end
+    let(:data) do
+      {
+        
+      }
+    end
+  end
 end
